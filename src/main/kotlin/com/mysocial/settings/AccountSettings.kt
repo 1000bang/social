30b2d7a612d40c22bf803e4207132198c206df5b
1000bang <1000_bang@naver.com>
2026-07-24 16:03:31 +0900

    fix: 기본 메시지 업데이트

--- src/main/kotlin/com/mysocial/settings/AccountSettings.kt
+++ src/main/kotlin/com/mysocial/settings/AccountSettings.kt
@@ -46,7 +46,7 @@ class AccountSettings(
 	val id: Long = 0
 
 	companion object {
-		const val DEFAULT_FOLLOW_PROMPT_TEXT = "댓글 남겨주셔서 감사합니다! 아래 버튼을 누르면 메시지가 발송돼요 😊"
+		const val DEFAULT_FOLLOW_PROMPT_TEXT = "{{사용자이름}}님, 댓글 남겨주셔서 감사합니다! 아래 버튼을 누르면 메시지가 발송돼요 😊"
 		const val DEFAULT_FOLLOW_BUTTON_TITLE = "메시지 보내주세요!"
 
 		const val DEFAULT_POST_PICKER_LIMIT = 5
